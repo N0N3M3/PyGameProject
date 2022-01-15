18ca883a4ffab8b397dd4fd69bf73affcc5d363d
VladimirSource <88400517+VladimirSource@users.noreply.github.com>
2022-01-15 14:31:42 +0300

    +яйцо

--- classes/egg.py
+++ classes/egg.py
@@ -3,12 +3,35 @@ import random
 
 
 class Egg:
-    def __init__(self, screen: pygame.Surface, width_screen, height_screen, speed):
+    def __init__(self, screen: pygame.Surface, width_screen, height_screen, speed: int, fps: int):
         self.screen = screen
         self.width_screen = width_screen
         self.height_screen = height_screen
         self.speed = speed
+        self.fps = fps
+        self.egg_size = 20
         self.side = random.randint(1, 4)
+        self.coords = None
+        self.delta_y = self.speed // self.fps
+        self._construct_egg()
 
     def _construct_egg(self):
-        ...
+        if self.side == 1:
+            coords = (0 + self.egg_size, int(self.height_screen * .18))
+        elif self.side == 2:
+            coords = (self.width_screen - self.egg_size, int(self.height_screen * .18))
+        elif self.side == 3:
+            coords = (0 + self.egg_size, int(self.height_screen * .45))
+        else:
+            coords = (self.width_screen - self.egg_size, int(self.height_screen * .45))
+        self.coords = coords
+
+        pygame.draw.circle(self.screen, pygame.Color(0, 0, 0), coords, self.egg_size)
+
+
+    def move(self, screen: pygame.Surface):
+        delta_x = self.speed // self.fps
+        delta_x = delta_x if self.side == 1 or self.side == 3 else -delta_x
+        self.coords = (self.coords[0] + delta_x, self.coords[1] + self.delta_y)
+        pygame.draw.circle(screen, pygame.Color(0, 0, 0), self.coords, self.egg_size)
+        return screen
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from classes.wolf import Wolf
-# from classes.egg import ..
+from classes.egg import Egg
 
 pygame.init()
 pygame.display.set_caption('Game')
@@ -8,27 +8,29 @@ size = width, height = 1400, 900
 screen = pygame.display.set_mode(size)
 
 running = True
-x_pos = 0
 v = 250  # пикселей в секунду
 fps = 60
 clock = pygame.time.Clock()
-screen.fill((0, 205, 0))
+screen.fill((255, 255, 255))
 wolf = Wolf(screen, width, height, 320)
+egg = Egg(screen, width, height, v, fps)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                wolf.set_position(4)
-            elif event.key == pygame.K_RIGHT:
-                wolf.set_position(2)
-            elif event.key == pygame.K_UP:
-                wolf.set_position(1)
-            elif event.key == pygame.K_DOWN:
-                wolf.set_position(3)
 
-    x_pos += v / fps
+            if event.key == pygame.K_LEFT:
+                screen = wolf.set_position(4, screen)
+            elif event.key == pygame.K_RIGHT:
+                screen = wolf.set_position(2, screen)
+            elif event.key == pygame.K_UP:
+                screen = wolf.set_position(1, screen)
+            elif event.key == pygame.K_DOWN:
+                screen = wolf.set_position(3, screen)
+    screen.fill((255, 255, 255))
+    screen = egg.move(screen)
+    screen = wolf.get_now_position(screen)
     clock.tick(fps)
     pygame.display.flip()
 pygame.quit()
